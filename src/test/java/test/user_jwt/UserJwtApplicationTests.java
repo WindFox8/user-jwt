f9e95ca57ed778368c2b324abbdbeb4e0b1e9109
WindFox8 <rafael_correr@hotmail.com>
2024-08-05 23:36:53 -0300

    initial commit

---
+++ src/test/java/test/user_jwt/UserJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package test.user_jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
